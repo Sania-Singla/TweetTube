9d5a256383223b3acfeb464d59eb47a0bfded4ff
Sania Singla <singlasania7717@gmail.com>
2025-01-03 15:52:22 +0530

    dotenv

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -57,10 +57,10 @@ app.use('/api/v1/about', aboutRouter);
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../Frontend/dist')));
     app.get('*', (req, res) => {
         res.sendFile(
-            path.resolve(__dirname, '..', 'client', 'dist', 'index.html')
+            path.resolve(__dirname, '..', 'Frontend', 'dist', 'index.html')
         );
     });
 }
